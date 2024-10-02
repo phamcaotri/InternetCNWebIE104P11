ba3fb6f957661ba99944a341428bca2820079bb6
phamcaotri <22521525@gm.uit.edu.vn>
2024-10-02 10:16:35 +0700

    Refactor Vite configuration and remove unused files

--- reelphim-app/vite.config.js
+++ reelphim-app/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, 'app'), // Đặt thư mục gốc là 'app'
   plugins: [react()],
-})
+  build: {
+    outDir: path.resolve(__dirname, 'dist'), // Đặt thư mục đầu ra là 'dist'
+  },
+  server: {
+    port: 3000, // Đặt cổng máy chủ phát triển
+  },
+});
