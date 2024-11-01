a0552db27f20d71ef6ed7e881f27437409e63801
phamcaotri <22521525@gm.uit.edu.vn>
2024-11-01 08:21:24 +0000

    Nov 1, 2024, 3:21 PM

--- reelphim-app/src/pages/HomePage.jsx
+++ reelphim-app/src/pages/HomePage.jsx
@@ -32,31 +32,31 @@ import { continueWatching, mostWatched, newReleases, recommended } from '../data
 //     </div>
 //   );
 // }
-// const HomePage = () => {
-//   return (
-//     <main>
-//       <MovieSection
-//         title="Tiếp tục xem"
-//         description="Tiếp tục hành trình khám phá những câu chuyện đang dang dở."
-//         movies={continueWatching}
-//       />
-//       <MovieSection
-//         title="Được xem nhiều nhất"
-//         description="Khám phá những bộ phim mà ai cũng đang bàn tán."
-//         movies={mostWatched}
-//       />
-//       <MovieSection
-//         title="Mới phát hành"
-//         description="Đừng bỏ lỡ cơ hội thưởng thức những bộ phim mới nhất."
-//         movies={newReleases}
-//       />
-//       <MovieSection
-//         title="Đề xuất"
-//         description="Khám phá những bộ phim mà bạn sẽ yêu thích."
-//         movies={recommended}
-//       />
-//     </main>
-//   );
-// };
+const HomePage = () => {
+  return (
+    <main>
+      <MovieSection
+        title="Tiếp tục xem"
+        description="Tiếp tục hành trình khám phá những câu chuyện đang dang dở."
+        movies={continueWatching}
+      />
+      <MovieSection
+        title="Được xem nhiều nhất"
+        description="Khám phá những bộ phim mà ai cũng đang bàn tán."
+        movies={mostWatched}
+      />
+      <MovieSection
+        title="Mới phát hành"
+        description="Đừng bỏ lỡ cơ hội thưởng thức những bộ phim mới nhất."
+        movies={newReleases}
+      />
+      <MovieSection
+        title="Đề xuất"
+        description="Khám phá những bộ phim mà bạn sẽ yêu thích."
+        movies={recommended}
+      />
+    </main>
+  );
+};
 
-// export default HomePage;
+export default HomePage;
