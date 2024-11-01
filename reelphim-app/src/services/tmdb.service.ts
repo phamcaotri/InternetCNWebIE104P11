7f694b72eb36d0ee0d1c0395bac1602c91c717fd
phamcaotri <22521525@gm.uit.edu.vn>
2024-11-01 14:51:47 +0000

    Nov 1, 2024, 9:51 PM

--- reelphim-app/src/services/tmdb.service.ts
+++ reelphim-app/src/services/tmdb.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { tmdbConfig } from '../config/tmdb.config';
-import { MovieResponse } from '../types/movie.types';
+import { MovieResponse, Movie } from '../types/movie.types';
 const tmdbApi = axios.create({
   baseURL: tmdbConfig.baseUrl,
   params: {
@@ -10,56 +10,56 @@ const tmdbApi = axios.create({
 
 export const tmdbService = {
   // Movies
-  getMoviesList: async (type, params) => {
+  getMoviesList: async (type: string, params: any): Promise<MovieResponse> => {
     const response = await tmdbApi.get(`/movie/${type}`, { params });
     return response.data;
   },
   
-  getMovieDetails: async (id) => {
+  getMovieDetails: async (id: number): Promise<Movie> => {
     const response = await tmdbApi.get(`/movie/${id}`);
     return response.data;
   },
   
   // TV Shows
-  getTvList: async (type, params) => {
+  getTvList: async (type: string, params: any): Promise<MovieResponse> => {
     const response = await tmdbApi.get(`/tv/${type}`, { params });
     return response.data;
   },
   
-  getTvDetails: async (id) => {
+  getTvDetails: async (id: number): Promise<Movie> => {
     const response = await tmdbApi.get(`/tv/${id}`);
     return response.data;
   },
   
   // Search
-  search: async (type, params) => {
+  search: async (type: string, params: any): Promise<MovieResponse> => {
     const response = await tmdbApi.get(`/search/${type}`, { params });
     return response.data;
   },
 
   // Categories/Genres
-  getGenres: async (mediaType) => {
+  getGenres: async (mediaType: string): Promise<any> => {
     const response = await tmdbApi.get(`/genre/${mediaType}/list`);
     return response.data;
   },
   // Trending
-  getTrending: async (mediaType, timeWindow = 'day', params = {}) => {
+  getTrending: async (mediaType: string, timeWindow: string = 'day', params: any = {}): Promise<any> => {
     const response = await tmdbApi.get(`/trending/${mediaType}/${timeWindow}`, { params });
     return response.data;
   },
 
   // Movies Extended
-  getNowPlayingMovies: async (params) => {
+  getNowPlayingMovies: async (params: any): Promise<any> => {
     const response = await tmdbApi.get('/movie/now_playing', { params });
     return response.data;
   },
 
-  getPopularMovies: async (params) => {
+  getPopularMovies: async (params: any): Promise<any> => {
     const response = await tmdbApi.get('/movie/popular', { params });
     return response.data;
   },
 
-  getUpcomingMovies: async (params) => {
+  getUpcomingMovies: async (params: any): Promise<any> => {
     const response = await tmdbApi.get('/movie/upcoming', { params });
     return response.data;
   },
