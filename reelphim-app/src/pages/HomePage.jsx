0826c806a9633dd6af923eba2037c26ee25d5a6a
phamcaotri <22521525@gm.uit.edu.vn>
2024-11-01 08:32:30 +0000

    Nov 1, 2024, 3:32 PM

--- reelphim-app/src/pages/HomePage.jsx
+++ reelphim-app/src/pages/HomePage.jsx
@@ -1,46 +1,35 @@
 import React from 'react';
 import MovieSection from '../components/MovieSection';
+import { continueWatching, mostWatched, newReleases, recommended } from '../data/homeData';
 import { useState, useEffect } from 'react';
 import { useTMDB } from '../hooks/useTMDB';
 
-function HomePage() {
-  const [trendingMovies, setTrendingMovies] = useState([]);
-  const { loading, error, fetchMovies } = useTMDB();
 
-  useEffect(() => {
-    const getMovies = async () => {
-      try {
-        const data = await fetchMovies('popular');
-        console.log('API Response:', data); // Thêm log để kiểm tra
-        
-        // Transform data để phù hợp với MovieCard component
-        const transformedMovies = data.results.map(movie => ({
-          title: movie.title,
-          year: new Date(movie.release_date).getFullYear(),
-          imageUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-        }));
-        
-        setTrendingMovies(transformedMovies);
-      } catch (err) {
-        console.error('Error fetching movies:', err);
-      }
-    };
-
-    getMovies();
-  }, [fetchMovies]);
-
-  if (loading) return <div className="text-text">Loading...</div>;
-  if (error) return <div className="text-text">Error: {error}</div>;
-
+const HomePage = () => {
   return (
-    <main className="px-4 py-8">
+    <main>
       <MovieSection
-        title="Test TMDB API"
-        description="Kiểm tra kết nối API"
-        movies={trendingMovies}
+        title="Tiếp tục xem"
+        description="Tiếp tục hành trình khám phá những câu chuyện đang dang dở."
+        movies={continueWatching}
+      />
+      <MovieSection
+        title="Được xem nhiều nhất"
+        description="Khám phá những bộ phim mà ai cũng đang bàn tán."
+        movies={mostWatched}
+      />
+      <MovieSection
+        title="Mới phát hành"
+        description="Đừng bỏ lỡ cơ hội thưởng thức những bộ phim mới nhất."
+        movies={newReleases}
+      />
+      <MovieSection
+        title="Đề xuất"
+        description="Khám phá những bộ phim mà bạn sẽ yêu thích."
+        movies={recommended}
       />
     </main>
   );
-}
+};
 
 export default HomePage;
