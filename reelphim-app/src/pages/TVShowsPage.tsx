019c7b636c8a5a2d28226f7a337514f92ed73692
PhanTruongThanhTrung <127408230+PhanTruongThanhTrung@users.noreply.github.com>
2024-11-17 18:38:19 +0700

    update api

--- reelphim-app/src/pages/TVShowsPage.tsx
+++ reelphim-app/src/pages/TVShowsPage.tsx
@@ -10,21 +10,27 @@ const TVShowsPage = () => {
   return (
     <main>
       <h1 className="text-3xl font-bold mb-8 text-text">TV Shows</h1>
+      {popularTVShows?.results && (
       <MovieSection
         title="Phổ biến"
         description="Những series TV đang được yêu thích nhất hiện nay."
         movies={popularTVShows}
       />
+      )}
+      {newTVShows?.results && (
       <MovieSection
         title="Mới phát hành"
         description="Các series TV mới nhất, đừng bỏ lỡ tập nào."
         movies={newTVShows}
       />
+      )}
+      {topRatedTVShows?.results && (
       <MovieSection
         title="Đánh giá cao nhất"
         description="Những series TV được đánh giá cao nhất mọi thời đại."
         movies={topRatedTVShows}
       />
+      )}
     </main>
   );
 };
